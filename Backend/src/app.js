2c1d423b3ec89aa315034580d4810a1aff2d9b0b
Shahbaz Ansari <116244599+Shahbaz9832@users.noreply.github.com>
2026-07-14 15:30:37 +0530

    Update app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,9 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173",
+             "https://ai-interview-preparation-platf-git-25c3c1-shahbaz9832s-projects.vercel.app"
+             ]
     credentials: true
 }))
 
